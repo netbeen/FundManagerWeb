02c143595f7d8ff03732ecf53bf32ac99b630fb7
舒达 <shuda.yy@alibaba-inc.com>
2016-08-30 11:33:26 +0800

    draw the first graph on page

--- static-assets/src/js/fund.js
+++ static-assets/src/js/fund.js
@@ -7,7 +7,89 @@ $(function() {
         url: '/api/v1/fund/'+this.innerHTML,
         type: "get",
       }).done(function(data) {
-        console.log(data);
+        var dates = Object.keys(data.values).reverse();
+        var fundValues = _.map(Object.keys(data.values),function(date){
+          return data.values[date];
+        }).reverse();
+        var unitPriceCompareOption = {
+          title: {
+            text: '成本净值对比图',
+            subtext: 'kkkkk'
+          },
+          tooltip: {
+            trigger: 'axis'
+          },
+          legend: {
+            data: ['单位净值', '持仓成本']
+          },
+          toolbox: {
+            show: true,
+            feature: {
+              dataView: {
+                show: true,
+                readOnly: false
+              },
+              restore: {
+                show: true
+              },
+              saveAsImage: {
+                show: true
+              }
+            }
+          },
+          calculable: true,
+          xAxis: [{
+            type: 'category',
+            boundaryGap: ['20%', '20%'],
+            data: dates
+          }],
+          yAxis: [{
+            type: 'value',
+            scale: true
+          }],
+          series: [{
+            name: '单位净值',
+            type: 'line',
+            data: fundValues,
+            markPoint: {
+              data: [{
+                symbolSize: 60,
+                type: 'max',
+                name: '最大值'
+              },
+                {
+                  symbolSize: 60,
+                  type: 'min',
+                  name: '最小值'
+                }]
+            },
+            markLine: {
+              data: [{
+                type: 'average',
+                name: '平均值'
+              }]
+            }
+          },
+            {
+              name: '持仓成本',
+              type: 'line',
+              data: fundValues,
+              markPoint: {
+                data: [{
+                  symbolSize: 60,
+                  type: 'max',
+                  name: '最大值'
+                },
+                  {
+                    symbolSize: 60,
+                    type: 'min',
+                    name: '最小值'
+                  }]
+              }
+            }]
+        };
+        var unitPriceCompare = echarts.init(document.getElementById('unitPriceCompare'));
+        unitPriceCompare.setOption(unitPriceCompareOption);
       });
     });
   }
