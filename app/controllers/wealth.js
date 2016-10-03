185e080d5edaad8bd9b5528bce865d0e0bbf1d66
舒达 <shuda.yy@alibaba-inc.com>
2016-10-03 21:22:14 +0800

    新建wealth页面

--- app/controllers/wealth.js
+++ app/controllers/wealth.js
@@ -2,10 +2,8 @@
 const purchaseInfoModel = require('../models/purchaseInfo');
 
 let render = async (ctx, next) => {
-  let fundIds = purchaseInfoModel.getFundIds();
-  ctx.render('fund',{
-    'pageName':'wealth',
-    'fundIds':fundIds
+  ctx.render('wealth',{
+    'pageName':'wealth'
   });
 };
 
