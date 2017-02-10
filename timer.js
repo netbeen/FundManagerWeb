92f904f32b3fb1a19709c4d53f590ba2701735d7
舒达 <shuda.yy@alibaba-inc.com>
2017-02-10 13:08:12 +0800

    增加昨日年华收益率

--- timer.js
+++ timer.js
@@ -69,31 +69,34 @@ const process = () => {
   let consoleResultData = [];
 
   var table = new Table({
-    head: ['ID', 'RTPPY', 'Name'],
-    colWidths: [8, 10, 26]
+    head: ['ID', 'PPY','RTPPY', 'Name'],
+    colWidths: [8, 10, 10, 26]
   });
 
   for (let id of ids){
     const chartData = fundService.getChartDataById(id);
 
-    let consoleResultItem = [id,chartData.overview.fundName];
+    let profitPerYear = chartData.profitsRatesPerYear[chartData.profitsRatesPerYear.length-1]
+    profitPerYear = profitPerYear<0?profitPerYear.toFixed(2).green:profitPerYear.toFixed(2).red;
+
+    let consoleResultItem = [id,profitPerYear,chartData.overview.fundName];
     table.push(consoleResultItem);
     consoleResultData.push(consoleResultItem);
 
     if(!chartData.overview.rtInfoValid){
       console.log(id,chartData.overview.fundName,'无法获取实时信息');
-      consoleResultItem.splice(1, 0, 'NRT');
+      consoleResultItem.splice(2, 0, 'NRT');
       continue;
     }
     let day = chartData.overview.rtTimeStamp.split(' ')[0].split('-')[2];
     if(parseInt(now.getDate()) !== parseInt(day)){
       console.log(id,chartData.overview.fundName,'当前时间为非交易时间');
-      consoleResultItem.splice(1, 0, 'NT');
+      consoleResultItem.splice(2, 0, 'NT');
       continue;
     }
     let displayRtProfitRatePerYear = chartData.overview.rtProfitRatePerYear<0?chartData.overview.rtProfitRatePerYear.toFixed(2).green:chartData.overview.rtProfitRatePerYear.toFixed(2).red;
     console.log(id,chartData.overview.fundName,'当前实时年化收益率: ',displayRtProfitRatePerYear,'%');
-    consoleResultItem.splice(1, 0, displayRtProfitRatePerYear+' %');
+    consoleResultItem.splice(2, 0, displayRtProfitRatePerYear+' %');
     if(chartData.dates[0]){
       const [year,month,day] = chartData.dates[0].split('-');
       const firstDate = new Date(parseInt(year),parseInt(month)-1,parseInt(day)+1);
