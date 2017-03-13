79a9dc19aedd7c3e76cca1618fc9cbcb42de04b7
舒达 <shuda.yy@alibaba-inc.com>
2017-03-13 11:52:18 +0800

    清理注释

--- app/models/wealthDistribution.js
+++ app/models/wealthDistribution.js
@@ -2,7 +2,6 @@
 const sqlite = require('sqlite-sync');
 
 sqlite.connect('./data/database.db');
-// console.log(sqlite.run('select * from distribute'));
 
 let getDistribution = () => {
   return sqlite.run('select * from distribution');
