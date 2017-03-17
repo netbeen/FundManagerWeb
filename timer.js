3dd35a36bfb66930e67c58586756dec3a28c25a1
舒达 <shuda.yy@alibaba-inc.com>
2017-03-17 11:37:56 +0800

    在timer里实现co异步封装

--- timer.js
+++ timer.js
@@ -4,7 +4,8 @@ const nodemailer = require('nodemailer');
 const fundService = require('./app/services/fund');
 const purchaseInfoModel = require('./app/models/purchaseInfo');
 const schedule = require('node-schedule');
-var Table = require('cli-table');
+const Table = require('cli-table');
+const co = require('co');
 
 const rtProfitRatePerYearThreshold = {
   '000071':15.0,
@@ -73,58 +74,59 @@ const process = () => {
     colWidths: [8, 10, 10, 26]
   });
 
-  for (let id of ids){
-    const chartData = fundService.getChartDataById(id);
+  co(function*(){
+    for (let id of ids){
+      const chartData = yield fundService.getChartDataById(id);
 
-    let profitPerYear = chartData.profitsRatesPerYear[chartData.profitsRatesPerYear.length-1]
-    profitPerYear = (profitPerYear<0?profitPerYear.toFixed(2).green:profitPerYear.toFixed(2).red) + ' %';
+      let profitPerYear = chartData.profitsRatesPerYear[chartData.profitsRatesPerYear.length-1]
+      profitPerYear = (profitPerYear<0?profitPerYear.toFixed(2).green:profitPerYear.toFixed(2).red) + ' %';
 
-    let consoleResultItem = [id,profitPerYear,chartData.overview.fundName];
-    table.push(consoleResultItem);
-    consoleResultData.push(consoleResultItem);
+      let consoleResultItem = [id,profitPerYear,chartData.overview.fundName];
+      table.push(consoleResultItem);
+      consoleResultData.push(consoleResultItem);
 
-    if(!chartData.overview.rtInfoValid){
-      console.log(id,chartData.overview.fundName,'无法获取实时信息');
-      consoleResultItem.splice(2, 0, 'NRT');
-      continue;
-    }
-    let day = chartData.overview.rtTimeStamp.split(' ')[0].split('-')[2];
-    if(parseInt(now.getDate()) !== parseInt(day)){
-      console.log(id,chartData.overview.fundName,'当前时间为非交易时间');
-      consoleResultItem.splice(2, 0, 'NT');
-      continue;
-    }
-    let displayRtProfitRatePerYear = chartData.overview.rtProfitRatePerYear<0?chartData.overview.rtProfitRatePerYear.toFixed(2).green:chartData.overview.rtProfitRatePerYear.toFixed(2).red;
-    console.log(id,chartData.overview.fundName,'当前实时年化收益率: ',displayRtProfitRatePerYear,'%');
-    consoleResultItem.splice(2, 0, displayRtProfitRatePerYear+' %');
-    if(chartData.dates[0]){
-      const [year,month,day] = chartData.dates[0].split('-');
-      const firstDate = new Date(parseInt(year),parseInt(month)-1,parseInt(day)+1);
-      const holdingDays = parseInt((now - firstDate)/1000/3600/24);
-      if(holdingDays < MININUM_HOLDING_DAYS){
-        console.log('持有时间未达到'+MININUM_HOLDING_DAYS+'天');
+      if(!chartData.overview.rtInfoValid){
+        console.log(id,chartData.overview.fundName,'无法获取实时信息');
+        consoleResultItem.splice(2, 0, 'NRT');
         continue;
       }
+      let day = chartData.overview.rtTimeStamp.split(' ')[0].split('-')[2];
+      if(parseInt(now.getDate()) !== parseInt(day)){
+        console.log(id,chartData.overview.fundName,'当前时间为非交易时间');
+        consoleResultItem.splice(2, 0, 'NT');
+        continue;
+      }
+      let displayRtProfitRatePerYear = chartData.overview.rtProfitRatePerYear<0?chartData.overview.rtProfitRatePerYear.toFixed(2).green:chartData.overview.rtProfitRatePerYear.toFixed(2).red;
+      console.log(id,chartData.overview.fundName,'当前实时年化收益率: ',displayRtProfitRatePerYear,'%');
+      consoleResultItem.splice(2, 0, displayRtProfitRatePerYear+' %');
+      if(chartData.dates[0]){
+        const [year,month,day] = chartData.dates[0].split('-');
+        const firstDate = new Date(parseInt(year),parseInt(month)-1,parseInt(day)+1);
+        const holdingDays = parseInt((now - firstDate)/1000/3600/24);
+        if(holdingDays < MININUM_HOLDING_DAYS){
+          console.log('持有时间未达到'+MININUM_HOLDING_DAYS+'天');
+          continue;
+        }
+      }
+      if(chartData.overview.rtProfitRatePerYear > rtProfitRatePerYearThreshold[id]){
+        console.log('收益率达到阈值'.red);
+        ifSendMail = true;
+        mailData.push({
+          id: id,
+          name: chartData.overview.fundName,
+          rtProfitRatePerYear: chartData.overview.rtProfitRatePerYear
+        });
+      }
     }
-    if(chartData.overview.rtProfitRatePerYear > rtProfitRatePerYearThreshold[id]){
-      console.log('收益率达到阈值'.red);
-      ifSendMail = true;
-      mailData.push({
-        id: id,
-        name: chartData.overview.fundName,
-        rtProfitRatePerYear: chartData.overview.rtProfitRatePerYear
-      });
-    }else{
+
+    console.log(table.toString());
+
+    if(ifSendMail){
+      const renderedMail = renderEmail(mailData);
+      console.log(renderedMail);
+      sendEmail(renderedMail);
     }
-  }
-
-  console.log(table.toString());
-
-  if(ifSendMail){
-    const renderedMail = renderEmail(mailData);
-    console.log(renderedMail);
-    sendEmail(renderedMail);
-  }
+  });
 };
 
 process();
