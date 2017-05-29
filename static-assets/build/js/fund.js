2968e78dd7db138c9c8e20619364d50278a1a340
舒达 <shuda.yy@alibaba-inc.com>
2017-05-29 17:14:30 +0800

    增加保存静态生成文件

---
+++ static-assets/build/js/fund.js
@@ -0,0 +1,278 @@
+$(function () {
+  if ($('#pageName').val() === 'fund') {
+
+    var lastElemStringify = function (array) {
+      var lastElem = array[array.length - 1];
+      var result = '';
+      if (lastElem >= 0) {
+        result = '+';
+      }
+      result += String(lastElem) + '%';
+      return result;
+    };
+
+    var percentagify = function (elem) {
+      var result = '';
+      if (elem >= 0) {
+        result = '+';
+      }
+      result += String(elem) + '%';
+      return result;
+    };
+
+    var setProfitRateStyleByArray = function (array, target) {
+      if (array[array.length - 1] < 0) {
+        target.addClass('green');
+        target.removeClass('red');
+      } else {
+        target.addClass('red');
+        target.removeClass('green');
+      }
+    };
+
+    var setProfitRateStyle = function (flag, target) {
+      if (flag < 0) {
+        target.addClass('green');
+        target.removeClass('red');
+      } else {
+        target.addClass('red');
+        target.removeClass('green');
+      }
+    };
+
+    var settingAccuracy = function (array, accuracy) {
+      return _.map(array, function (elem) {
+        return elem.toFixed(accuracy);
+      });
+    };
+
+    $('#fund-id-list > .list-group-item').on('click', function () {
+      $('#fund-id-list > .list-group-item').removeClass('active');
+      $(this).addClass('active');
+      $.ajax({
+        url: '/api/v1/fund/' + $(this).attr('value'),
+        type: "get",
+      }).done(function (chartData) {
+        console.log(chartData);
+
+        $('#pruchaseInfoButton').on('click',() => {
+          $('.base').html(chartData.overview.base);
+          let deficit = chartData.overview.totalCost - parseFloat(chartData.overview.currentPrice)
+          deficit = deficit > 0 ? deficit:0;
+          $('.deficit').html(deficit.toFixed(2));
+          let total = chartData.overview.base + (deficit*2);
+          $('.total').html(total.toFixed(2));
+          $('#purchaseInfoModal').modal('show');
+        });
+
+        chartData.userPrices = settingAccuracy(chartData.userPrices, 4);
+        chartData.profitRates = settingAccuracy(chartData.profitRates, 2);
+        chartData.profitsRatesPerYear = settingAccuracy(chartData.profitsRatesPerYear, 2);
+        chartData.overview.currentPrice = chartData.overview.currentPrice.toFixed(2);
+        chartData.overview.rtProfitRate = chartData.overview.rtInfoValid?chartData.overview.rtProfitRate.toFixed(2):'null';
+        chartData.overview.rtProfitRatePerYear = chartData.overview.rtInfoValid?chartData.overview.rtProfitRatePerYear.toFixed(2):'null';
+
+        $('#fundId').html(chartData.fundId);
+        $('#fundName').html(chartData.overview.fundName);
+        $('#totalCost').html(chartData.overview.totalCost);
+        $('#currentPrice').html(chartData.overview.currentPrice);
+        $('#profitRate').html(lastElemStringify(chartData.profitRates));
+        $('#profitRatePerYear').html(lastElemStringify(chartData.profitsRatesPerYear));
+        $('#rtProfitRate').html(percentagify(chartData.overview.rtProfitRate) + ' ( ' + chartData.overview.rtTimeStamp + ' )');
+        $('#rtProfitRatePerYear').html(percentagify(chartData.overview.rtProfitRatePerYear));
+
+        setProfitRateStyleByArray(chartData.profitRates, $('#profitRate'));
+        setProfitRateStyleByArray(chartData.profitsRatesPerYear, $('#profitRatePerYear'));
+        setProfitRateStyle(chartData.overview.rtProfitRate, $('#rtProfitRate'));
+        setProfitRateStyle(chartData.overview.rtProfitRatePerYear, $('#rtProfitRatePerYear'));
+
+        if (chartData.overview.trading === true) {
+          $('#overview .trading').removeClass('hide');
+          $('#overview .untrading').addClass('hide');
+        }else{
+          $('#overview .trading').addClass('hide');
+          $('#overview .untrading').removeClass('hide');
+        }
+
+        var dates = chartData.dates;
+        var unitPrices = chartData.unitPrices;
+        var userPrices = chartData.userPrices;
+        var profitRates = chartData.profitRates;
+        var profitsRatesPerYear = chartData.profitsRatesPerYear;
+        var toolboxConfig = {
+          show: true,
+          feature: {
+            dataView: {
+              show: true,
+              readOnly: false
+            },
+            restore: {
+              show: true
+            },
+            saveAsImage: {
+              show: true
+            }
+          }
+        };
+        var markPointConfig = {
+          data: [{
+            symbolSize: 60,
+            type: 'max',
+            name: '最大值'
+          },
+            {
+              symbolSize: 60,
+              type: 'min',
+              name: '最小值'
+            }]
+        };
+        let tooltipConfig = {
+          trigger: 'axis',
+          axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+          }
+        };
+        var priceCompareOption = {
+          title: {
+            text: '成本净值对比图',
+            subtext: chartData.fundId
+          },
+          tooltip: tooltipConfig,
+          legend: {
+            data: ['单位净值', '持仓成本']
+          },
+          toolbox: toolboxConfig,
+          calculable: true,
+          xAxis: [{
+            type: 'category',
+            boundaryGap: ['20%', '20%'],
+            data: dates
+          }],
+          yAxis: [{
+            type: 'value',
+            scale: true
+          }],
+          series: [{
+            name: '单位净值',
+            type: 'line',
+            data: unitPrices,
+            markPoint: markPointConfig,
+            markLine: {
+              data: [{
+                type: 'average',
+                name: '平均值'
+              }]
+            }
+          },
+            {
+              name: '持仓成本',
+              type: 'line',
+              data: userPrices,
+              markPoint: markPointConfig
+            }]
+        };
+        var profitRateOption = {
+          color: ['#61a0a8'],
+          title: {
+            text: '收益率示意图',
+            subtext: chartData.fundId
+          },
+          tooltip: tooltipConfig,
+          legend: {
+            data: ['收益率%']
+          },
+          toolbox: toolboxConfig,
+          calculable: true,
+          xAxis: [{
+            show: false,
+            type: 'category',
+            boundaryGap: ['20%', '20%'],
+            data: dates
+          }],
+          yAxis: [{
+            type: 'value',
+            scale: true,
+            axisLabel: {
+              formatter: function (v) {
+                return v + '%'
+              }
+            }
+          }],
+          series: [{
+            name: '收益率%',
+            type: 'line',
+            data: profitRates,
+            markLine: {
+              data: [
+                [{
+                  name: '0%收益率线',
+                  coord: [dates[0], 0]
+                }, {
+                  coord: [dates[dates.length - 1], 0]
+                }
+                ]]
+            },
+            markPoint: markPointConfig,
+          }]
+        };
+
+        var profitRatePerYearCompareOption = {
+          color: ['#dd8668'],
+          title: {
+            text: '年化收益率示意图',
+            subtext: chartData.fundId
+          },
+          tooltip: tooltipConfig,
+          legend: {
+            data: ['赎回年化收益率%']
+          },
+          toolbox: toolboxConfig,
+          calculable: true,
+          xAxis: [{
+            show: false,
+            type: 'category',
+            boundaryGap: ['20%', '20%'],
+            data: dates
+          }],
+          yAxis: [{
+            type: 'value',
+            scale: true,
+            axisLabel: {
+              formatter: function (v) {
+                return v + '%'
+              }
+            }
+          }],
+          series: [
+            {
+              name: '赎回年化收益率%',
+              type: 'line',
+              data: profitsRatesPerYear,
+              markLine: {
+                data: [
+                  [{
+                    name: '0%收益率线',
+                    coord: [dates[0], 0]
+                  }, {
+                    coord: [dates[dates.length - 1], 0]
+                  }
+                  ]]
+              },
+              markPoint: markPointConfig
+            }]
+        };
+
+        var priceCompare = echarts.init(document.getElementById('priceCompare'));
+        priceCompare.setOption(priceCompareOption);
+
+        var profitRate = echarts.init(document.getElementById('profitRateCompare'));
+        profitRate.setOption(profitRateOption);
+
+        var profitRateComparePerYear = echarts.init(document.getElementById('profitRatePerYearCompare'));
+        profitRateComparePerYear.setOption(profitRatePerYearCompareOption);
+
+        echarts.connect([priceCompare, profitRate, profitRateComparePerYear]);
+      });
+    });
+  }
+});
---
+++ static-assets/build/js/wealth.js
@@ -0,0 +1,238 @@
+$(function () {
+  if ($('#pageName').val() === 'wealth') {
+
+    $.ajax({
+      url: '/api/v1/wealthDistribution',
+      type: "get",
+    }).done(function (data) {
+      let total = [];
+      let distribution = {};
+      let distributionPercentage = {};
+      let datesEchartFormat = [];
+
+      // 遍历ajax请求所获取的数组信息
+      _.each(data,function(elem){
+        const currentDate = new Date(elem['日期']);
+        datesEchartFormat.push([currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate()].join('/'));
+        let currentTotal = 0;
+        _.each(Object.keys(elem),function(key){
+          if(key!=='日期'){
+            currentTotal += elem[key];
+            if(key in distribution){
+              distribution[key].push(elem[key]);
+            }else{
+              distribution[key] = [elem[key]];
+            }
+          }
+        });
+        total.push({
+          name: _.last(datesEchartFormat),
+          value:[_.last(datesEchartFormat),currentTotal.toFixed(2)]
+        });
+
+        _.each(Object.keys(distribution),(key)=>{
+          if(key in distributionPercentage){
+            distributionPercentage[key].push({
+              name: _.last(datesEchartFormat),
+              value:[_.last(datesEchartFormat),(_.last(distribution[key])/currentTotal*100).toFixed(2)]
+            });
+          }else{
+            distributionPercentage[key]=[{
+              name: _.last(datesEchartFormat),
+              value:[_.last(datesEchartFormat),(_.last(distribution[key])/currentTotal*100).toFixed(2)]
+            }];
+          }
+        });
+      });
+
+
+      console.log('distribution',distribution);
+      console.log('distributionPercentage',distributionPercentage);
+      console.log('total',total);
+
+      const distributionChartOptionSeries = [{
+        name:'总金额',
+        type:'line',
+        animation: true,
+        smooth: true,
+        lineStyle: {
+          normal: {
+            width: 5,
+          },
+        },
+        data:total,
+        markArea: {
+          silent: true,
+          data: [[{
+            xAxis: '2014/1/1',
+            itemStyle: {
+              normal: {
+                color: '#dddddd',
+                opacity: 0.5
+              }
+            }
+          }, {
+            xAxis: '2014/12/31'
+          }],[{
+            xAxis: '2016/1/1',
+            itemStyle: {
+              normal: {
+                color: '#dddddd',
+                opacity: 0.5
+              }
+            }
+          }, {
+            xAxis: '2016/12/31'
+          }]]
+        },
+      }];
+      const distributionChartOptionLegendData = ['总金额'];
+
+      _.each(Object.keys(distributionPercentage),(keyName)=>{
+        distributionChartOptionSeries.push({
+          name:keyName+'%',
+          type:'line',
+          yAxisIndex:1,
+          animation: true,
+          smooth: true,
+          lineStyle: {
+            normal: {
+              width: 3,
+            },
+          },
+          data: distributionPercentage[keyName]
+        });
+        distributionChartOptionLegendData.push(keyName+'%');
+      });
+
+
+      const distributionChartOption = {
+        title : {
+          text: '资产分布趋势',
+          x: 'left',
+          align: 'right'
+        },
+        grid: {
+          bottom: 80
+        },
+        tooltip : {
+          trigger: 'axis',
+          axisPointer: {
+            animation: false
+          }
+        },
+        legend: {
+          data:distributionChartOptionLegendData,
+          x: 'right'
+        },
+        dataZoom: [
+          {
+            show: true,
+            realtime: true,
+            start: 0,
+            end: 100
+          }
+        ],
+        xAxis : [
+          {
+            type : 'time',
+            boundaryGap : false,
+            axisLine: {onZero: false},
+          }
+        ],
+        yAxis: [
+          {
+            name: '总资产(RMB/元)',
+            type: 'value',
+          },
+          {
+            name: '百分比(%)',
+            type: 'value',
+          }
+        ],
+        series: distributionChartOptionSeries
+      };
+      const distributionChart = echarts.init(document.getElementById('wealthDistribution'),'shine');
+      distributionChart.setOption(distributionChartOption);
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+      //接下来处理饼图
+
+      let typeMap = {};
+      let legendData = [];
+      $.ajax({
+        url: '/api/v1/wealthType',
+        type: "get",
+      }).done((typeObjs) => {
+        _.each(typeObjs, (typeObj) => {
+          if(!(typeObj.type in typeMap)){
+            typeMap[typeObj.type] = [typeObj.target];
+          }else{
+            typeMap[typeObj.type].push(typeObj.target);
+          }
+        });
+
+        let typePieData = [];
+        let targetPieData = [];
+        _.each(Object.keys(typeMap),(type)=>{
+          legendData.push(type);
+          let currentTypeTotal = 0;
+          _.each(Object.keys(typeMap[type]),(target)=>{
+            legendData.push(typeMap[type][target]);
+            currentTypeTotal += _.last(distribution[typeMap[type][target]]);
+            targetPieData.push({value:_.last(distribution[typeMap[type][target]]), name:typeMap[type][target]});
+          });
+          typePieData.push({value:currentTypeTotal, name:type});
+        });
+
+
+        const pieOption = {
+          title : {
+            text: '资产分布比例',
+            x: 'left',
+            align: 'right',
+            top:'15%'
+          },
+          tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
+          },
+          legend: {
+            orient: 'vertical',
+            x: 'left',
+            bottom: '24%',
+            data:legendData
+          },
+          series: [
+            {
+              name:'类型',
+              type:'pie',
+              selectedMode: 'single',
+              radius: [0, '40%'],
+
+              label: {
+                normal: {
+                  position: 'inner'
+                }
+              },
+              labelLine: {
+                normal: {
+                  show: false
+                }
+              },
+              data:typePieData
+            },
+            {
+              name:'资产分布',
+              type:'pie',
+              radius: ['45%', '60%'],
+              data:targetPieData
+            }
+          ]
+        };
+        const distributionPieChart = echarts.init(document.getElementById('currentDistributionPie'),'shine');
+        distributionPieChart.setOption(pieOption);
+      });
+    });
+  }
+});
