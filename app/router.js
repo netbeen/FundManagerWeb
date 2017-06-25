54c18dec11429ba27964fa71591a4e47febf6f32
舒达 <shuda.yy@alibaba-inc.com>
2017-06-25 20:47:20 +0800

    增加登录页

--- app/router.js
+++ app/router.js
@@ -5,16 +5,14 @@ const wealthController = require('./controllers/wealth');
 const apiFundController = require('./controllers/api/fund');
 const apiWealthController = require('./controllers/api/wealth');
 
-router.get('/fund', fundController.render);
-router.get('/wealth', wealthController.render);
-router.get('/login', wealthController.login);
-
 router.get('/api/v1/fund/:id',apiFundController.getGraphInfo);
 router.get('/api/v1/fundIds',apiFundController.getFundIds);
 
 router.get('/api/v1/wealthDistribution',apiWealthController.getWealthDistribution);
 router.get('/api/v1/wealthType',apiWealthController.getTypes);
 
-
+router.get('/fund', fundController.render);
+router.get('/wealth', wealthController.render);
+router.get('/login', wealthController.login);
 
 module.exports = router;
