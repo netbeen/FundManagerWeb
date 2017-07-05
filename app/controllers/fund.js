d9c014545fb5daf9c21f0a886380a64e4c5e4d2f
舒达 <shuda.yy@alibaba-inc.com>
2017-07-05 23:01:51 +0800

    取消cookie鉴权

--- app/controllers/fund.js
+++ app/controllers/fund.js
@@ -2,9 +2,9 @@
 const purchaseInfoModel = require('../models/purchaseInfo');
 
 let render = async (ctx) => {
-  if(typeof ctx.cookies.get('uuid') === 'undefined'){
-    ctx.redirect('/login');
-  }
+  // if(typeof ctx.cookies.get('uuid') === 'undefined'){
+  //   ctx.redirect('/login');
+  // }
   let fundIds = purchaseInfoModel.getFundIds();
   ctx.render('fund',{
     'pageName':'fund',
